9baf0a5231307539971eeaa31521434aa6032cfe
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-21 09:35:21 +0300

    Added create user test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,4 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model(self)
+        user = User.objects.create_user(
+            username='will', email='will@email.com', password='testpass123'
+        )
+        self.assertEqual(user.username, 'will')
+        self.assetEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
