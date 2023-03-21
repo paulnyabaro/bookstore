10e4840b04a837a1f325deb7d209b4d16bb33d52
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-21 14:23:47 +0300

    Created admin user create tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
-        User = get_user_model(self)
+        User = get_user_model()
         user = User.objects.create_user(
             username='will', email='will@email.com', password='testpass123'
         )
@@ -15,7 +15,7 @@ class CustomUserTests(TestCase):
         self.assertFalse(user.is_superuser)
 
     def test_create_superuser(self):
-        User = get_user_model(self)
+        User = get_user_model()
         admin_user = User.objects.create_superuser(
             username='superadmin', email='superadmin@email.com', password='testpass123'
         )
