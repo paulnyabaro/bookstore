9f9ecb612ccadbc40b6a5111445541a059c09986
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-21 15:22:10 +0300

    Working on homepage tests

--- pages/tests.py
+++ pages/tests.py
@@ -6,3 +6,7 @@ class HomepageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
