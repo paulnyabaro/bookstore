4e831616632d7bc6feabec91fe5ee4727f0f3b93
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-22 09:32:35 +0300

    Added resolve

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,7 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import IndexView
 
 
 class HomepageTests(SimpleTestCase):
@@ -10,9 +12,6 @@ class HomepageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         self.assertEqual(self.response.status_code, 200)
 
-    # def test_homepage_url_name(self):
-    #     self.assertEqual(self.response.status_code, 200)
-
     def test_homepage_template(self):
         self.assertTemplateUsed(self.response, 'index.html')
 
@@ -21,3 +20,7 @@ class HomepageTests(SimpleTestCase):
 
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(self.response, 'This is not on the page')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, IndexView.as_view().__name__)
