e6eff12e9db85ee4acd48a752bc510cc7c6dc375
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-22 15:49:12 +0300

    Added accounts urls

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # Apps
+    # Local
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
 ]
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
 ]
