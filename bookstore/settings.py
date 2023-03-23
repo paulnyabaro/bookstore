d8e3827d7695c4c50dd9445a4bb398b59fff3fad
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-23 06:59:57 +0300

    Added static root directory

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = [BASE_DIR / 'staticfiles'] # Where static files will be stored after collectstatic is run
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
