d43082b3b02383bacbd04070c9b209223734a02b
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-23 06:16:23 +0300

    Added signup form test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .forms import CustomUserCreationForm
+from .views import SignupPageView
 
 
 class CustomUserTests(TestCase):
@@ -37,3 +40,8 @@ class SignupPageTests(TestCase):
         self.assertTemplateUsed(self.response, 'registration/signup.html')
         self.assertContains(self.response, 'Sign Up')
         self.assertNotContains(self.response, 'This is random text that should not be there')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
