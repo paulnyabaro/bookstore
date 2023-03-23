905b79e008b9589181750a60d268a2110d56fec6
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-23 09:16:41 +0300

    Added about page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import AboutView, IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
 class IndexView(TemplateView):
     template_name = 'index.html'
