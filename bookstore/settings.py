9d0709070b682b71f67c173a79e574d27ed64f2a
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-24 18:07:02 +0300

    Added authentication backend

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -53,6 +53,10 @@ INSTALLED_APPS = [
 # Django-allauth config
 SITE_ID = 1
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend'
+)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
