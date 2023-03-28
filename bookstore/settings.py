d9bb393f2544cda42bd9c726ac0af44024d8cfb4
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-28 12:11:12 +0300

    Updating database settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -91,14 +91,8 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 # Using Postgresql database
 DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": 5432,
-    }
+    'default': env.dj_db_url('DATABASE_URL',
+    default='postgres://postgres@db/postgres')
 }
 
 
