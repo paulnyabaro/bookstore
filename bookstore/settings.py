fc470a8fc24c3419ae29b15bf8edc5951e550c14
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-28 06:52:05 +0300

    Updated secret key on settings file

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=e@0#)whgi5w$%m2j-s@tbidmv*^&$mh9(-e)rn$p20o^oheqc'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
