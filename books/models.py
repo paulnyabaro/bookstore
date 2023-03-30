2db38282664e77d83ef747dca1b8564015073829
paulnyabaro <paulnyabaro1@gmail.com>
2023-03-30 06:15:43 +0300

    Created books migrations

--- books/models.py
+++ books/models.py
@@ -21,3 +21,6 @@ class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
     review = models.CharField(max_length=255)
     author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review
