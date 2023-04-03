7218da5b1320efdd8f3883fdc9916c5b9617ff3b
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-03 11:15:00 +0300

    Updated static files

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles' # Where static files will be stored after collectstatic is run
-STATICFILES_STORAGE = 'whitenoise.storate.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
